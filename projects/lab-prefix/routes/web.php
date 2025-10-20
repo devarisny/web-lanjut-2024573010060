4839f5ab451b8ecc93d79016fd3311524aeefb55
Deva Risny <risnydeva@gmail.com>
2025-10-20 09:10:12 +0700

    lab-prefix

---
+++ projects/lab-prefix/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\UserController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::prefix('admin')->group(function () {
+  Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+  Route::get('/users', [UserController::class, 'index'])->name('admin.users');
+  Route::get('/users/{id}', [UserController::class, 'show'])->name('admin.users.show');  
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
